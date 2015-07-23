5e12d40e4b99db22bd26cdb67968869ef9cea8f2
qaribhaider <qaribhaider@gmail.com>
2015-07-23 14:24:10 +0400

    Release v1.0.1
    Added comments

--- download.php
+++ download.php
@@ -1,14 +1,23 @@
 <?php
 
+// Include Class
 include './class.download.php';
 
+// Set files to download
 $files_to_download = array(
     "http://lorempixel.com/output/nightlife-q-g-640-480-4.jpg",
     "http://lorempixel.com/output/abstract-q-c-640-222-1.jpg",
     "http://lorempixel.com/output/sports-h-g-640-800-7.jpg"
 );
 
+// Initialize
 $md = new MultiDownload();
+
+// Set dir/path where you want files to be downloaded
 $md->setSavePath("./downloads/");
+
+// Point URLs
 $md->setURLs($files_to_download);
+
+// Finally, download files
 $md->download();
